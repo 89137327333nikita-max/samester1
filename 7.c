55ca4ab421b78862c3c687da5b12b29e82337589
89137327333nikita-max <89137327333nikita@gmail.com>
2025-10-17 01:35:39 +0700

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <float.h>
+int main (void)
+{
+float numf = 1.0/3.0;
+double numd = 1.0/3.0;
+printf("float: %.4f; double: %.4f \n", numf, numd);
+printf("float: %.12f; double: %.12f \n", numf, numd);
+printf("float: %.17f; double: %.17f \n", numf, numd);
+printf("%d %d", FLT_DIG, DBL_DIG);
+getchar();
+return 0;
